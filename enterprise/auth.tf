98ab141190427d1186968722b674a91681b28d00
Yoko Hyakuna <yoko@hashicorp.com>
2022-04-07 21:01:32 -0700

    Initial commit

---
+++ enterprise/auth.tf
@@ -0,0 +1,38 @@
+#--------------------------------
+# Enable userpass auth method
+#--------------------------------
+resource "vault_auth_backend" "userpass" {
+  type = "userpass"
+}
+
+# Create a user named, "student"
+resource "vault_generic_endpoint" "student" {
+  depends_on           = [vault_auth_backend.userpass]
+  path                 = "auth/userpass/users/student"
+  ignore_absent_fields = true
+
+  data_json = <<EOT
+{
+  "policies": ["fpe-client", "admins"],
+  "password": "changeme"
+}
+EOT
+}
+
+#--------------------------------------------------------------------
+# Enable approle auth method in the 'education/training' namespace
+#--------------------------------------------------------------------
+resource "vault_auth_backend" "approle" {
+  depends_on = [vault_namespace.training]
+  provider   = vault.training
+  type       = "approle"
+}
+
+# Create a role named, "test-role"
+resource "vault_approle_auth_backend_role" "tester" {
+  depends_on     = [vault_auth_backend.approle]
+  provider   = vault.training
+  backend        = vault_auth_backend.approle.path
+  role_name      = "test-role"
+  token_policies = ["default", "admins"]
+}
